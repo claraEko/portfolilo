66a2ea11154fdc5dcd6d7b80a1543557f9f02956
claradelvecchio <clara.anquetil@studentfr.ch>
2025-04-03 17:07:50 +0200

    star animation

--- src/js/main.js
+++ src/js/main.js
@@ -6,10 +6,22 @@ function bigRotateStars() {
   void bigStar.offsetWidth;
   bigStar.classList.add("is-active");
 }
+function bigZoomStars() {
+  bigStar.classList.remove("zoom");
+  void bigStar.offsetWidth;
+  bigStar.classList.add("zoom");
+}
 function smalRotateStars() {
   smalStar.classList.remove("is-active");
   void bigStar.offsetWidth;
   smalStar.classList.add("is-active");
 }
+function smalZoomStars() {
+  smalStar.classList.remove("zoom");
+  void bigStar.offsetWidth;
+  smalStar.classList.add("zoom");
+}
 bigStar.addEventListener("mouseenter", bigRotateStars);
+bigStar.addEventListener("click", bigZoomStars);
 smalStar.addEventListener("mouseenter", smalRotateStars);
+smalStar.addEventListener("click", smalZoomStars);
