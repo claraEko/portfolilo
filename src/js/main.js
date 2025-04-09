126830d2b46b48f1c62fa42dfbbfd6f482ba0499
claradelvecchio <clara.anquetil@studentfr.ch>
2025-04-09 11:50:35 +0200

    scroll base

--- src/js/main.js
+++ src/js/main.js
@@ -25,3 +25,35 @@ bigStar.addEventListener("mouseenter", bigRotateStars);
 bigStar.addEventListener("click", bigZoomStars);
 smalStar.addEventListener("mouseenter", smalRotateStars);
 smalStar.addEventListener("click", smalZoomStars);
+
+const dots = document.querySelectorAll("#dot ul li");
+const sections = [
+  document.querySelector("#aboutme"),
+  document.querySelector("#customize-product-card"),
+  document.querySelector("#logo-type-card"),
+];
+
+window.addEventListener("scroll", () => {
+  let currentIndex = -1;
+
+  sections.forEach((section, index) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+
+    if (window.scrollY >= sectionTop + window.innerHeight / 2) {
+      currentIndex = index;
+    }
+  });
+
+  if (currentIndex !== -1) {
+    dots.forEach((dot) => dot.classList.remove("active"));
+    dots[currentIndex].classList.add("active");
+  }
+});
+
+dots.forEach((dot, index) => {
+  dot.addEventListener("click", (e) => {
+    e.preventDefault();
+    sections[index].scrollIntoView({ behavior: "smooth" });
+  });
+});
